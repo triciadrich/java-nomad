4d3f5becb599a152a7b1d3f51e1f470bd5bc3318
triciadrich <36164322+triciadrich@users.noreply.github.com>
2022-06-18 10:28:59 -0700

    fund

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -13,7 +13,24 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         int i = 1;
+        boolean bool = true;
+        char ch = 'A';
+        byte by = 127;
+        short sh = 32000;
+        long lon = 9000000;
+        float fl = 32.3f;
+        double dob = 999.999d;
+
+
+
         System.out.println("int i is: " + i);
+        System.out.println("boolean bool is: " + bool);
+        System.out.println("char ch is: " + ch);
+        System.out.println("byte by is: " + by);
+        System.out.println("short sh is: " + sh);
+        System.out.println("long lon is: " + lon);
+        System.out.println("float fl is: " + fl);
+        System.out.println("double dob is: " + dob);
 
         // write your code below
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,20 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
+        int st = str.length();
+        System.out.println("st is: " + st);
         String str2 = "hello";
+
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean bool = str.equals(str2);
+        System.out.println("bool is: " + bool);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String str3 = str + str2;
+        System.out.println(str3);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        int search = str3.indexOf("!");
+        System.out.println("search: " + search);
     }
 
 
