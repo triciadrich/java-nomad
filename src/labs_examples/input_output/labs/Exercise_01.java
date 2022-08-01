7b17ece713b56177bd8909f875404e289b4a9f8b
triciadrich <36164322+triciadrich@users.noreply.github.com>
2022-07-31 21:48:04 -0700

    i/o

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -10,8 +12,35 @@ package labs_examples.input_output.labs;
  */
 
 class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        FileOutputStream fileOutputStream = null;
 
+
+        try{
+            inputStream = new FileInputStream("C:\\Users\\Owner\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\char_data.txt");
+            bufferedInputStream = new BufferedInputStream(inputStream);
+            fileOutputStream =  new FileOutputStream("C:\\Users\\Owner\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\ex1Copy.txt");
+
+            byte[] buffer = new byte[5];
+
+
+            while((bufferedInputStream.read(buffer)) != -1){
+
+                fileOutputStream.write(buffer);
+            }
+        } catch (IOException exc){
+            System.out.println("Error " + exc.getMessage());
+        }finally {
+            try{
+                inputStream.close();
+                bufferedInputStream.close();
+                fileOutputStream.close();
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
 
 
     }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -12,3 +14,38 @@ package labs_examples.input_output.labs;
  *
  */
 
+public class Exercise_02 {
+    public static void main(String[] args) {
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+
+        try{
+            reader = new BufferedReader(new FileReader("C:\\Users\\Owner\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\char_data.txt"));
+            writer = new PrintWriter(new FileWriter("C:\\Users\\Owner\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\ex2copy.txt"));
+            int s;
+
+            while((s = reader.read()) != -1){
+                char c =(char) s;
+                if ( s == 'a'){
+                    c = '-';
+                    writer.print(c);
+                } else if (s == 'e') {
+                    c = '~';
+                    writer.print(c);
+                }else {
+                    writer.print(c);
+                }
+            }
+        }catch (IOException exc){
+            System.out.println("Error: " + exc.getMessage());
+        }finally {
+            try{
+                reader.close();
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+            writer.close();
+        }
+    }
+}
